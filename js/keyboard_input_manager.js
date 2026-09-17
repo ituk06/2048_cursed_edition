f58ca3fb3b3c87843e1817ba76a5dbe7e0d424dc
Ituk <125300866+ituk06@users.noreply.github.com>
2026-09-17 16:39:06 +0000

    commit button code

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -139,6 +139,6 @@ KeyboardInputManager.prototype.keepPlaying = function (event) {
 
 KeyboardInputManager.prototype.bindButtonPress = function (selector, fn) {
   var button = document.querySelector(selector);
-  button.addEventListener("click", fn.bind(this));
-  button.addEventListener(this.eventTouchend, fn.bind(this));
+  //button.addEventListener("click", fn.bind(this));
+  //button.addEventListener(this.eventTouchend, fn.bind(this));
 };
